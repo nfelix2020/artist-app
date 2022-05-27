8efd8e509512c429c1066a3e828990f592fe2519
Félix NSHIMIYIMANA <nfelix@yahoo.com>
2022-05-27 13:52:04 +0200

    upload image

--- src/app/new-artist/new-artist.component.ts
+++ src/app/new-artist/new-artist.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewArtistComponent implements OnInit {
 
+  url = 'https://img.icons8.com/ios/100/000000/contract-job.png';
+
+  onSelect(event: any){
+    if(event.target.files[0]){
+      let reader= new FileReader()
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload= (event: any) =>{
+        this.url= event.target.result;
+      }
+    }
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
